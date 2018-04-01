18d6d34886d278c3a663dd3f3e770055b84a37e9
apimenov <alxpimenov@gmail.com>
2018-04-01 22:09:42 +0300

    Proper board size

--- src/main/java/com/github/apimenov/game/of/rooks/Board.java
+++ src/main/java/com/github/apimenov/game/of/rooks/Board.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
 
 public class Board {
 
-    static final int SIZE = 6;
+    static final int SIZE = 8;
 
     private BoardCell[][] cells = new BoardCell[SIZE][SIZE];
 
